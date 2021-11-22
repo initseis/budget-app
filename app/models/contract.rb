e256043860e41b7e01283c4aa0c247fe255cd48b
Kevin <kevinsevinche@gmail.com>
2021-11-21 22:29:43 -0500

    Add group and user models

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,4 @@
-class Group < ApplicationRecord
+class Contract < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_contracts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :groups, foreign_key: 'author_id', class_name: 'Group', dependent: :destroy
+  has_many :contracts, foreign_key: 'author_id', class_name: 'Contract', dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
