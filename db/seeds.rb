edcf91045ab7d10a017f023c345930bbd822252c
Kevin <kevinsevinche@gmail.com>
2021-11-21 19:45:23 -0500

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user_1 = User.create(name: "Kevin", email: "kevin@gmail.com", password: "234234")
+@group_1 = Group.create(name: "clothes", icon: "https://www.svgrepo.com/show/275006/shirt-clothes.svg", author_id: @user_1.id)
