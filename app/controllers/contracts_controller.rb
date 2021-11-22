edacd1a0fa9adb20e42d284a81949ff258ee7dd2
Kevin <kevinsevinche@gmail.com>
2021-11-21 23:53:24 -0500

    Add contracts controller

---
+++ app/controllers/contracts_controller.rb
@@ -0,0 +1,33 @@
+class ContractsController < ApplicationController
+  def new
+    @contract = current_user.contracts.new
+    @groups = current_user.groups
+  end
+
+  def create
+    begin
+      if @contract = current_user.contracts.create(contract_params)
+        params[:groups].each do |group|
+          throw error unless @contract.group_contracts.create(group_id: group)
+            
+          # else
+            
+          # end
+        end
+        flash[:notice] = 'Contract added successfully'
+        redirect_to groups_url
+      else
+        throw error
+      end
+    rescue
+      flash[:error] = 'Contract couldn\'t be added'
+      render :new
+    end
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:name, :amount)
+  end
+end
