9a6c8dcecce504ec9374ab6226055b47c7370b12
Kevin <kevinsevinche@gmail.com>
2021-11-21 23:54:03 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'groups/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :groups, except: [:delete]
+  resources :groups, except: [:delete] do
+    resources :contracts, except: [:delete]
+  end 
   root to: "groups#index"
 end
