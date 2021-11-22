c843001a346f56eed5645ea745737bab5082e209
Kevin <kevinsevinche@gmail.com>
2021-11-22 10:20:00 -0500

    Add javascript function to validate contract checkboxes

---
+++ app/javascript/packs/form_validation.js
@@ -0,0 +1,31 @@
+const contractForm = document.querySelector(".add-contract-form");
+const checkboxes = contractForm.querySelectorAll("#groups_");
+const checkboxLength = checkboxes.length;
+const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+
+function init() {
+  if (firstCheckbox) {
+    for (let i = 0; i < checkboxLength; i++) {
+      checkboxes[i].addEventListener("change", checkValidity);
+    }
+
+    checkValidity();
+  }
+}
+
+function isChecked() {
+  for (let i = 0; i < checkboxLength; i++) {
+    if (checkboxes[i].checked) return true;
+  }
+
+  return false;
+}
+
+function checkValidity() {
+  const errorMessage = !isChecked()
+    ? "At least one checkbox must be selected."
+    : "";
+  firstCheckbox.setCustomValidity(errorMessage);
+}
+
+init();
