ec8b060eab450b5437ff889ba1f246e4355711c1
Kevin <kevinsevinche@gmail.com>
2021-11-22 23:47:06 -0500

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 @user_1 = User.create(name: "Kevin", email: "kevin@gmail.com", password: "234234")
 @group_1 = Group.create(name: "clothes", icon: "https://www.svgrepo.com/show/275006/shirt-clothes.svg", author_id: @user_1.id)
-@group_2 = Group.create(name: "foods", icon: "https://www.svgrepo.com/show/275006/shirt-clothes.svg", author_id: @user_1.id)
+@group_2 = Group.create(name: "foods", icon: "https://www.svgrepo.com/show/366737/food.svg", author_id: @user_1.id)
 @contract_1 = Contract.create(name: "Rockys", amount: 60.5, author_id: @user_1.id)
 @contract_2 = Contract.create(name: "Starbucks", amount: 20.3, author_id: @user_1.id)
 @contract_3 = Contract.create(name: "Kadosh", amount: 25, author_id: @user_1.id)
