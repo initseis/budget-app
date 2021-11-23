640eb80b9f12cd026c4c953a6c872a8599e3dbfc
Kevin <kevinsevinche@gmail.com>
2021-11-22 23:46:01 -0500

    Update models

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,6 @@
 class Contract < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_contracts
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_integer: false }
 end
